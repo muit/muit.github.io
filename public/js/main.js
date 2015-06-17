8d68dce2cdf26781b7264a02bd062684f6e447c5
muit <miguel_3c@hotmail.com>
2015-06-17 23:45:30 +0200

    Added Copyright

--- public/js/main.js
+++ public/js/main.js
@@ -70,18 +70,6 @@ $(document).ready(function() {
         $window    = $(window),
         offset     = $sidebar.offset(),
         topPadding = 0;
-
-    $window.scroll(function() {
-      if ($window.scrollTop() > offset.top) {
-        $sidebar.stop().animate({
-          marginTop: $window.scrollTop() - offset.top + topPadding
-        });
-      } else {
-        $sidebar.stop().animate({
-          marginTop: 0
-        });
-      }
-    });
 });
 
 loadBackground();
