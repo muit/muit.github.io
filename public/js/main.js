1809d6d7a58948f522d1cb5edd6a5e8a61190160
muit <miguel_3c@hotmail.com>
2015-06-25 17:06:34 +0200

    Server & fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,102 +1,103 @@
-
 http = {
-	getJSON: function(url, callback){
-		var xhr = this.createCORSRequest("GET", url);
-		var self = this;
+  getJSON: function(url, callback) {
+    var xhr = this.createCORSRequest("GET", url);
+    var self = this;
 
-		xhr.onload = function() {
-		    var data = {};
-			if (request.status >= 200 && request.status < 400) {
-			// Success!
-			data = JSON.parse(request.responseText);
-			} else {
-				data.e = request.status;
-			}
+    xhr.onload = function() {
+      var data = {};
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        data = JSON.parse(request.responseText);
+      } else {
+        data.e = request.status;
+      }
 
-			callback(data);
-		};
+      callback(data);
+    };
 
-		xhr.onerror = function(error) {
-			callback({e: error});
-		};
+    xhr.onerror = function(error) {
+      callback({
+        e: error
+      });
+    };
 
-		xhr.withCredentials = true;
-		xhr.send();
-	},
+    xhr.withCredentials = true;
+    xhr.send();
+  },
 
 
 
-	createCORSRequest: function(method, url) {
-		var xhr = new XMLHttpRequest();
-		if ("withCredentials" in xhr)
-			xhr.open(method, url, true);
-		else if (typeof XDomainRequest != "undefined") {
-			xhr = new XDomainRequest();
-			xhr.open(method, url);
-		} else
-			xhr = null;
-		return xhr;
-	}
+  createCORSRequest: function(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr)
+      xhr.open(method, url, true);
+    else if (typeof XDomainRequest != "undefined") {
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+    } else
+      xhr = null;
+    return xhr;
+  }
 }
 
 
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-      anchors:['titleSection', 'contentSection'],
-      /*autoScrolling: mobilecheck(),*/
-      responsiveHeight: 700,
+  $('#fullpage').fullpage({
+    anchors: ['titleSection', 'contentSection'],
+    /*autoScrolling: mobilecheck(),*/
+    //responsiveHeight: 700,
 
-      fixedElements: '#slidemenu',
+    fixedElements: '#slidemenu',
 
-      touchSensitivity: 30,
+    touchSensitivity: 15,
 
-      //Design
-      controlArrows: false,
-      verticalCentered: false,
-      resize : true,
+    //Design
+    controlArrows: false,
+    verticalCentered: false,
+    resize: true,
 
-      afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
-        if(index  == 2){
-          $('.section').eq(index-1).find('.toSlide').removeClass('active').eq(slideIndex).addClass('active');  
-        }
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+      if (index == 2) {
+        $('.section').eq(index - 1).find('.toSlide').removeClass('active').eq(slideIndex).addClass('active');
       }
-    });
-
-    //PreFix to the white border
-    window.onscroll = function(){
-      window.scrollBy(-300,0);
     }
+  });
 
-    $('.toSlide').click(function () {
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
-      var index = $(this).index();
-      $.fn.fullpage.moveTo("contentSection", index);
+  //PreFix to the white border
+  window.onscroll = function() {
+    window.scrollBy(-300, 0);
+  }
 
-      moveArrow(index);
-    });
-    setTimeout(function(){
-      moveArrow($('.toSlide.active').index());
-    }, 2500);
+  $('.toSlide').click(function() {
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    var index = $(this).index();
+    $.fn.fullpage.moveTo("contentSection", index);
 
-    var $sidebar   = $(".nav-bar.top"), 
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 0;
+    moveArrow(index);
+  });
+  setTimeout(function() {
+    moveArrow($('.toSlide.active').index());
+  }, 2500);
+
+  var $sidebar = $(".nav-bar.top"),
+    $window = $(window),
+    offset = $sidebar.offset(),
+    topPadding = 0;
 });
 
-function moveArrow(index){
+function moveArrow(index) {
   var arrow = $("#arrow");
   arrow.removeClass("offset_1");
   arrow.removeClass("offset_6");
   arrow.removeClass("offset_10");
-  var offset = "offset_"+((index == 2)?"10":((index == 1)?"6":"1"));
+  var offset = "offset_" + ((index == 2) ? "10" : ((index == 1) ? "6" : "1"));
   arrow.addClass(offset);
 }
 
 loadBackground();
 
-function loadBackground(){
+function loadBackground() {
   //------------------------------
   // FFS Background
   //------------------------------
@@ -191,7 +192,7 @@ function loadBackground(){
       output.removeChild(renderer.element);
     }
 
-    switch(index) {
+    switch (index) {
       case WEBGL:
         renderer = webglRenderer;
         break;
@@ -279,7 +280,7 @@ function loadBackground(){
   }
 
   function update() {
-    var ox, oy, oz, l, light, v, vertex, offset = MESH.depth/2;
+    var ox, oy, oz, l, light, v, vertex, offset = MESH.depth / 2;
 
     // Update Bounds
     FSS.Vector3.copy(LIGHT.bounds, center);
@@ -293,8 +294,8 @@ function loadBackground(){
       ox = Math.sin(LIGHT.step[0] * now * LIGHT.speed);
       oy = Math.cos(LIGHT.step[1] * now * LIGHT.speed);
       FSS.Vector3.set(attractor,
-        LIGHT.bounds[0]*ox,
-        LIGHT.bounds[1]*oy,
+        LIGHT.bounds[0] * ox,
+        LIGHT.bounds[1] * oy,
         LIGHT.zOffset);
     }
 
@@ -328,9 +329,9 @@ function loadBackground(){
       oy = Math.cos(vertex.time + vertex.step[1] * now * MESH.speed);
       oz = Math.sin(vertex.time + vertex.step[2] * now * MESH.speed);
       FSS.Vector3.set(vertex.position,
-        MESH.xRange*geometry.segmentWidth*ox,
-        MESH.yRange*geometry.sliceHeight*oy,
-        MESH.zRange*offset*oz - offset);
+        MESH.xRange * geometry.segmentWidth * ox,
+        MESH.yRange * geometry.sliceHeight * oy,
+        MESH.zRange * offset * oz - offset);
       FSS.Vector3.add(vertex.position, vertex.anchor);
     }
 
@@ -348,7 +349,7 @@ function loadBackground(){
         light = scene.lights[l];
         lx = light.position[0];
         ly = light.position[1];
-        switch(RENDER.renderer) {
+        switch (RENDER.renderer) {
           case CANVAS:
             renderer.context.lineWidth = 0.5;
             renderer.context.beginPath();
@@ -411,6 +412,8 @@ function loadBackground(){
 
 window.mobilecheck = function() {
   var check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+  (function(a) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
+  })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 }
