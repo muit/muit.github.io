b8fbe710f7fc6e2a4f3d784622402ea73ab61fda
muit <muit@piperift.com>
2017-01-31 22:47:53 +0100

    Added More Content, Visual Improvements
    - Added Global Game Jam 2017
    - Fixed Carroussel switch
    - Added custom mobile layout
    - Fixed Image sizes and naming
    - More visual fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(window).scroll(function(event) {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
+    
+    var scrollPos = $(document).scrollTop();
+    $('.nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        //600 is a height offset
+        if (refElement.position().top-600 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav li').removeClass("active");
+            currLink.parent().addClass("active");
+        }
+        else{
+            //currLink.parent().removeClass("active");
+        }
+    });
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -17,10 +32,9 @@ $(function() {
         	top = offset? offset.top : 0;
         }
         $("ul.nav.navbar-nav").children('li').each(function () {
-            console.log(this.value);
-            $(this).removeClass("active");
+            //$(this).removeClass("active");
         });
-        $anchor.parent().addClass("active");
+        //$anchor.parent().addClass("active");
         
         $('html, body').stop().animate({
             scrollTop: top-75
