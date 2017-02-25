c3a6ee26153f53c75f63f97d5ad325a0331aa644
muit <muit@piperift.com>
2017-02-25 17:25:05 +0100

    Cleaned site, Added modals and comparison images

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@ $(window).scroll(function(event) {
     });
 });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         var top = 0;
@@ -41,9 +42,7 @@ $(function() {
         }, 700);
         event.preventDefault();
     });
-});
 
-$(function() {
     $('a.page-fast-scroll').bind('click', function(event) {
         var $anchor = $(this);
         var top = 0;
@@ -56,4 +55,133 @@ $(function() {
         }, 500);
         event.preventDefault();
     });
+
+    
+});
+
+jQuery(document).ready(function($){
+    var dragging = false,
+        scrolling = false,
+        resizing = false;
+
+    //cache jQuery objects
+    var comparisonContainers = $('.comp-container');
+
+    //check if the .comp-container is in the viewport 
+    //if yes, animate it
+    checkPosition(comparisonContainers);
+    $(window).on('scroll', function(){
+        if( !scrolling) {
+            scrolling =  true;
+            ( !window.requestAnimationFrame )
+                ? setTimeout(function(){checkPosition(comparisonContainers);}, 100)
+                : requestAnimationFrame(function(){checkPosition(comparisonContainers);});
+        }
+    });
+    
+    //make the .bar element draggable and modify .left-img width according to its position
+    comparisonContainers.each(function(){
+        var actual = $(this);
+        drags(actual.find('.bar'), actual.find('.left-img'), actual, actual.find('.label[data-type="original"]'), actual.find('.label[data-type="modified"]'));
+    });
+
+    //update images label visibility
+    $(window).on('resize', function(){
+        if( !resizing) {
+            resizing =  true;
+            ( !window.requestAnimationFrame )
+                ? setTimeout(function(){checkLabel(comparisonContainers);}, 100)
+                : requestAnimationFrame(function(){checkLabel(comparisonContainers);});
+        }
+    });
+
+    function checkPosition(container) {
+        container.each(function(){
+            var actualContainer = $(this);
+            if( $(window).scrollTop() + $(window).height()*0.5 > actualContainer.offset().top) {
+                actualContainer.addClass('is-visible');
+            }
+        });
+
+        scrolling = false;
+    }
+
+    function checkLabel(container) {
+        container.each(function(){
+            var actual = $(this);
+            updateLabel(actual.find('.label[data-type="modified"]'), actual.find('.left-img'), 'left');
+            updateLabel(actual.find('.label[data-type="original"]'), actual.find('.left-img'), 'right');
+        });
+
+        resizing = false;
+    }
+
+    //draggable funtionality - credits to http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
+    function drags(dragElement, resizeElement, container, labelContainer, labelResizeElement) {
+        dragElement.on("mousedown vmousedown touchstart", function(e) {
+            dragElement.addClass('draggable');
+            resizeElement.addClass('resizable');
+
+            var x = e.pageX? e.pageX : e.originalEvent.touches[0].clientX;
+
+            var dragWidth = dragElement.outerWidth(),
+                xPosition = dragElement.offset().left + dragWidth - x,
+                containerOffset = container.offset().left,
+                containerWidth = container.outerWidth(),
+                minLeft = containerOffset + 10,
+                maxLeft = containerOffset + containerWidth - dragWidth - 10;
+            
+            dragElement.parents().on("mousemove vmousemove touchmove", function(e) {
+                if( !dragging) {
+                    dragging =  true;
+                    ( !window.requestAnimationFrame )
+                        ? setTimeout(function(){animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement);}, 100)
+                        : requestAnimationFrame(function(){animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement);});
+                }
+            }).on("mouseup vmouseup touchend", function(e){
+                dragElement.removeClass('draggable');
+                resizeElement.removeClass('resizable');
+            });
+            e.preventDefault();
+        }).on("mouseup vmouseup touchend", function(e) {
+            dragElement.removeClass('draggable');
+            resizeElement.removeClass('resizable');
+        });
+    }
+
+    function animateDraggedHandle(e, xPosition, dragWidth, minLeft, maxLeft, containerOffset, containerWidth, resizeElement, labelContainer, labelResizeElement) {
+        var x = e.pageX? e.pageX : e.originalEvent.touches[0].clientX;
+        
+        var leftValue = x + xPosition - dragWidth;   
+        //constrain the draggable element to move inside his container
+        if(leftValue < minLeft ) {
+            leftValue = minLeft;
+        } else if ( leftValue > maxLeft) {
+            leftValue = maxLeft;
+        }
+
+        var widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
+        
+        $('.draggable').css('left', widthValue).on("mouseup vmouseup touchend", function() {
+            $(this).removeClass('draggable');
+            resizeElement.removeClass('resizable');
+        });
+
+        $('.resizable').css('width', widthValue); 
+
+        updateLabel(labelResizeElement, resizeElement, 'left');
+        updateLabel(labelContainer, resizeElement, 'right');
+        dragging =  false;
+    }
+
+    function updateLabel(label, resizeElement, position) {
+        if(label.offset() == undefined)
+            return;
+
+        if(position == 'left') {
+            ( label.offset().left + label.outerWidth() < resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
+        } else {
+            ( label.offset().left > resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
+        }
+    }
 });
